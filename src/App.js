2d964b6ef46fbdbf37ad25b80548abe5248fd41d
Bateyjosue <josuebatey19@gmail.com>
2022-09-13 22:26:40 +0200

    Revert "Add the Browser Route"
    This reverts commit 194f0bbc3c3052b2bc730d6d29a4d05a503cf009.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
-import Home from './components/Home';
-import Quote from './components/Quote';
-
-import style from './components/styles/App.module.css';
 
 class App extends Component {
   constructor(props) {
@@ -16,19 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className={style.flex}>
-          <h1>Math Magicians</h1>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/calculator">Calculate</Link>
-            <Link to="/quote">Quote</Link>
-          </nav>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="calculator" element={<Calculator />} />
-          <Route path="quote" element={<Quote />} />
-        </Routes>
+        <Calculator />
       </div>
     );
   }
