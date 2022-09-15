0fbf2baf0c909425ae5bfa038a8e1fbdc65ed768
Bateyjosue <josuebatey19@gmail.com>
2022-09-15 17:22:42 +0200

    Add style to the apps and the content as well

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,18 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import style from './styles/Quote.module.css';
 
+const highlight = {
+  background: 'rgb(218, 51, 17)',
+};
 export default function Quote() {
   return (
     <main className={style.maincontainer}>
       <p className={style.par}>
-        Mathematics is not about numbers, equations, computations, or
-        algorithms; it is about understanding. __ William Paul Thurston
+        Mathematics is not about <span style={highlight}>numbers</span>,
+        {' '}<span style={highlight}>equations</span>,{' '}
+        <span style={highlight}>computations</span> , or <span style={highlight}>algorithms</span>;
+        it is about understanding. __ William Paul Thurston
       </p>
     </main>
   );
