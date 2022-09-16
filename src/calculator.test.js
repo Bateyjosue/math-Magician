6a7fa0b783c3892e6d93a2982770b89d8b3a12d4
Mirwais Farahi <mirfarahi92@gmail.com>
2022-09-16 13:26:00 +0500

    Edit calculator.test.js file for calculate test

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -25,3 +25,37 @@ describe('Modulus Testing', () => {
     expect(operate(12, 6, '%')).toBe('0');
   });
 });
+
+import calculate from '../logic/calculate';
+
+describe('Test calculate.js', () => {
+  it('Test Operation Function', () => {
+    const testObj = {
+      total: 0,
+      next: 4,
+      operation: '+',
+    };
+    const calculation = calculate(testObj, '+');
+    expect(calculation.operation).toBe('+');
+  });
+
+  it('Test total', () => {
+    const testObj = {
+      total: 20,
+      next: 4,
+      operation: '+',
+    };
+    const calculation = calculate(testObj, '=');
+    expect(calculation.total).toBe('24');
+  });
+
+  it('Test total', () => {
+    const testObj = {
+      total: 20,
+      next: 40,
+      operation: '-',
+    };
+    const calculation = calculate(testObj, '=');
+    expect(calculation.total).toBe('-20');
+  });
+});
